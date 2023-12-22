464bd9b54e65d6e25ee66c0db5d16c29e9cc2140
takuro-inoue <inoue.takuro@moneyforward.co.jp>
2023-12-22 21:15:46 +0900

    Update Firebase authentication and add async storage dependency

--- firebase.ts
+++ firebase.ts
@@ -1,16 +1,19 @@
 import { initializeApp, FirebaseOptions } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseOptions: FirebaseOptions = {
-  apiKey: "AIzaSyDVGSixjE_nqfFlUFh59xWiS90qC1M1CSE",
-  authDomain: "refrigerator-manage.firebaseapp.com",
-  projectId: "refrigerator-manage",
-  storageBucket: "refrigerator-manage.appspot.com",
-  messagingSenderId: "458856160759",
-  appId: "1:458856160759:web:656fb97fe0e247041462f2",
-  measurementId: "G-MGWYPE9EG3"
+  apiKey: 'AIzaSyDVGSixjE_nqfFlUFh59xWiS90qC1M1CSE',
+  authDomain: 'refrigerator-manage.firebaseapp.com',
+  projectId: 'refrigerator-manage',
+  storageBucket: 'refrigerator-manage.appspot.com',
+  messagingSenderId: '458856160759',
+  appId: '1:458856160759:web:656fb97fe0e247041462f2',
+  measurementId: 'G-MGWYPE9EG3',
 };
 
 const app = initializeApp(firebaseOptions);
 
-export const auth = getAuth(app);
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
