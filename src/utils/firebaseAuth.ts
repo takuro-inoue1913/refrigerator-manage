d9136b72b6553c3d685bb72ac6bb2b3cd61ca8d7
takuro-inoue <inoue.takuro@moneyforward.co.jp>
2023-12-23 21:36:54 +0900

    Add Firebase environment variables and configuration

--- src/utils/firebaseAuth.ts
+++ src/utils/firebaseAuth.ts
@@ -1,15 +1,16 @@
 import { initializeApp, FirebaseOptions } from 'firebase/app';
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import env from '@env';
 
 const firebaseOptions: FirebaseOptions = {
-  apiKey: 'AIzaSyDVGSixjE_nqfFlUFh59xWiS90qC1M1CSE',
-  authDomain: 'refrigerator-manage.firebaseapp.com',
-  projectId: 'refrigerator-manage',
-  storageBucket: 'refrigerator-manage.appspot.com',
-  messagingSenderId: '458856160759',
-  appId: '1:458856160759:web:656fb97fe0e247041462f2',
-  measurementId: 'G-MGWYPE9EG3',
+  apiKey: env.FIREBASE_API_KEY,
+  authDomain: env.FIREBASE_AUTH_DOMAIN,
+  projectId: env.FIREBASE_PROJECT_ID,
+  storageBucket: env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: env.FIREBASE_APP_ID,
+  measurementId: env.FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseOptions);
