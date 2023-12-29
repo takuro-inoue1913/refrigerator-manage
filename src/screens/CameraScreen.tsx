8fcead0840339a45a92c1d831a3b4d072348e338
takuro-inoue <inoue.takuro@moneyforward.co.jp>
2023-12-29 17:52:28 +0900

    Add Google Cloud Vision API key and implement image analysis

--- src/screens/CameraScreen.tsx
+++ src/screens/CameraScreen.tsx
@@ -1,15 +1,61 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Linking, StyleSheet } from 'react-native';
 import { Camera } from 'expo-camera';
 import { Button, Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+import { GOOGLE_CLOUD_VISION_API_KEY } from '@env';
 import { LinearGradientButton } from '@src/components/GradationButton';
 
 // import { LoadingScreen } from '@src/screens/LoadingScreen';
 
 export const CameraScreen = () => {
   const [permission, requestPermission] = Camera.useCameraPermissions();
+  const cameraRef = useRef<Camera>(null);
+
+  const analyzeImageWithGoogleVision = async (base64: string) => {
+    try {
+      const body = JSON.stringify({
+        requests: [
+          {
+            features: [{ type: 'TEXT_DETECTION', maxResults: 1 }],
+            image: {
+              content: base64,
+            },
+          },
+        ],
+      });
+
+      const response = await fetch(
+        `https://vision.googleapis.com/v1/images:annotate?key=${GOOGLE_CLOUD_VISION_API_KEY}`, // ここにあなたのAPIキーを入れてください
+        {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          method: 'POST',
+          body: body,
+        },
+      );
+
+      const responseJson = await response.json();
+      console.log(
+        'textAnnotations.description: ',
+        responseJson.responses?.[0].textAnnotations?.[0].description as string,
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const takePicture = async () => {
+    const photo = await cameraRef.current?.takePictureAsync({
+      base64: true,
+    });
+    if (photo && photo.base64) {
+      analyzeImageWithGoogleVision(photo.base64);
+    }
+  };
 
   useEffect(() => {
     if (permission?.granted) {
@@ -33,10 +79,10 @@ export const CameraScreen = () => {
 
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera} />
+      <Camera style={styles.camera} ref={cameraRef} />
       <View style={styles.footerContainer}>
         <LinearGradientButton
-          onPress={() => console.log('pressed')}
+          onPress={takePicture}
           width={80}
           height={80}
           borderRadius={50}
@@ -63,6 +109,7 @@ const styles = StyleSheet.create({
   footerContainer: {
     flex: 1,
     flexDirection: 'row',
+    justifyContent: 'center',
     paddingBottom: 50,
     paddingTop: 20,
   },
