125c541eab691a4456744f89ad9d1a4e3a144509
takuro-inoue <inoue.takuro@moneyforward.co.jp>
2023-12-31 12:16:26 +0900

    Add HASURA_ADMIN_SECRET environment variable

--- types/env.d.ts
+++ types/env.d.ts
@@ -7,4 +7,5 @@ declare module '@env' {
   export const FIREBASE_APP_ID: string;
   export const FIREBASE_MEASUREMENT_ID: string;
   export const GOOGLE_CLOUD_VISION_API_KEY: string;
+  export const HASURA_ADMIN_SECRET: string;
 }
