4ec1ea19d39fdf6e24b253bf270c2698ef4fb23f
takuro-inoue <inoue.takuro@moneyforward.co.jp>
2024-01-01 22:21:37 +0900

    Add touch overlay and handle press event in ItemImage component

--- src/components/ItemImage.tsx
+++ src/components/ItemImage.tsx
@@ -1,5 +1,12 @@
 import React, { useState, ComponentProps, FC } from 'react';
-import { Image, StyleSheet } from 'react-native';
+import {
+  GestureResponderEvent,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import { SkeletonImage } from '@src/components/common/SkeletonImage';
 import commonStyle from '@src/utils/commonStyle';
@@ -21,21 +28,62 @@ type Props = {
  */
 export const ItemImage: FC<Props> = (props) => {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [touchedSide, setTouchedSide] = useState<'left' | 'right' | null>(null);
+  const [overlayOpacity, setOverlayOpacity] = useState(0);
+
+  const handlePress = (event: GestureResponderEvent) => {
+    const touchX = event.nativeEvent.locationX;
+    const imageHalfWidth = commonStyle.image.width / 2;
+    const newTouchedSide = touchX < imageHalfWidth ? 'left' : 'right';
+    setTouchedSide(newTouchedSide);
+    setOverlayOpacity(0);
+    setTimeout(() => setOverlayOpacity(1), 100);
+    // TODO: ここでAPIを叩く。
+    setTimeout(() => {
+      setTouchedSide(null);
+    }, 200);
+  };
 
   return (
     <>
       {!isLoaded && <SkeletonImage />}
-      <Image
-        {...props}
-        onLoadEnd={() => setIsLoaded(true)}
-        style={[
-          props.hasStock ? styles.activeImage : styles.image,
-          isLoaded ? styles.imageVisible : styles.imageHidden,
-        ]}
-      />
-      {isLoaded && props.hasStock && (
-        <ItemBadge quantity={props.quantity} unitName={props.unitName} />
-      )}
+      <TouchableOpacity activeOpacity={1} onPress={handlePress}>
+        <Image
+          {...props}
+          onLoadEnd={() => setIsLoaded(true)}
+          style={[
+            props.hasStock ? styles.activeImage : styles.image,
+            isLoaded ? styles.imageVisible : styles.imageHidden,
+          ]}
+        />
+        {isLoaded && touchedSide && (
+          <View
+            style={[
+              touchedSide === 'left' ? styles.overlayLeft : styles.overlayRight,
+              {
+                opacity: overlayOpacity,
+              },
+            ]}
+            pointerEvents="none"
+          >
+            <Icon
+              name={touchedSide === 'left' ? 'minus' : 'plus'}
+              size={35}
+              color="#fff"
+              // 下側の中央寄せにする
+              style={{
+                position: 'absolute',
+                bottom: 0,
+                left: touchedSide === 'left' ? 0 : undefined,
+                right: touchedSide === 'right' ? 0 : undefined,
+              }}
+            />
+          </View>
+        )}
+        {isLoaded && props.hasStock && (
+          <ItemBadge quantity={props.quantity} unitName={props.unitName} />
+        )}
+      </TouchableOpacity>
     </>
   );
 };
@@ -56,5 +104,25 @@ const styles = StyleSheet.create({
   },
   imageHidden: {
     opacity: 0,
+  },
+  overlayLeft: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    width: commonStyle.image.width / 2,
+    backgroundColor: 'rgba(0,0,0,0.3)',
+    borderTopLeftRadius: 15,
+    borderBottomLeftRadius: 15,
+  },
+  overlayRight: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    width: commonStyle.image.width / 2,
+    backgroundColor: 'rgba(0,0,0,0.3)',
+    borderTopRightRadius: 15,
+    borderBottomRightRadius: 15,
   },
 });
