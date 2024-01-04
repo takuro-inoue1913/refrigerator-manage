efa039d70ff8b81b77f326346394cb724134ca4c
takuro-inoue <inoue.takuro@moneyforward.co.jp>
2024-01-04 09:47:33 +0900

    Add meat stock actions

---
+++ src/states/meat/action.ts
@@ -0,0 +1,82 @@
+import { useRecoilCallback } from 'recoil';
+import { MeatStocks, meatStocksState } from '@src/states/meat';
+
+type MeatStockActions = {
+  increaseMeatStock: ({
+    meatId,
+    quantity,
+  }: {
+    /** 増やす肉ID */
+    meatId: number;
+    /** 増やす数を指定。 */
+    quantity: number;
+  }) => void;
+  decreaseMeatStock: ({
+    meatId,
+    quantity,
+  }: {
+    /** 減らす肉ID */
+    meatId: number;
+    /** 減らす数を指定。 */
+    quantity: number;
+  }) => void;
+};
+
+export const useMeatStockActions = () => {
+  const increaseMeatStock: MeatStockActions['increaseMeatStock'] =
+    useRecoilCallback(
+      ({ set }) =>
+        ({ meatId, quantity }) => {
+          set(meatStocksState, (prev) => {
+            const newStocks: MeatStocks = {
+              ids: [...prev.ids],
+              byId: prev.ids.reduce(
+                (acc, cur) => {
+                  acc[cur] = { ...prev.byId[cur] };
+                  if (cur === meatId) {
+                    acc[cur].quantity += quantity;
+                    acc[cur].hasStock = true;
+                  }
+                  return acc;
+                },
+                {} as MeatStocks['byId'],
+              ),
+            };
+            return newStocks;
+          });
+        },
+      [],
+    );
+
+  const decreaseMeatStock: MeatStockActions['decreaseMeatStock'] =
+    useRecoilCallback(
+      ({ set }) =>
+        ({ meatId, quantity }) => {
+          set(meatStocksState, (prev) => {
+            const updatedQuantity = prev.byId[meatId].quantity - quantity;
+
+            const newStocks: MeatStocks = {
+              ids: [...prev.ids],
+              byId: prev.ids.reduce(
+                (acc, cur) => {
+                  acc[cur] = { ...prev.byId[cur] };
+                  if (cur === meatId) {
+                    acc[cur].quantity = Math.max(updatedQuantity, 0);
+                    acc[cur].hasStock = updatedQuantity > 0;
+                  }
+                  return acc;
+                },
+                {} as MeatStocks['byId'],
+              ),
+            };
+            return newStocks;
+          });
+        },
+      [],
+    );
+
+  return {
+    increaseMeatStock,
+    decreaseMeatStock,
+  };
+};
