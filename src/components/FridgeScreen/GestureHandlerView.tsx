2a03d0983abdd08b982bd4ad8e117f4b4a832b1d
takuro-inoue <inoue.takuro@moneyforward.co.jp>
2024-01-07 12:36:23 +0900

    Remove unused imports and update swipe threshold

--- src/components/FridgeScreen/GestureHandlerView.tsx
+++ src/components/FridgeScreen/GestureHandlerView.tsx
@@ -1,7 +1,6 @@
 import { selectFridgeCategoryState } from '@src/states/fridge';
 import { selectItems } from '@src/utils/consts';
 import React, { FC } from 'react';
-import { Alert, Dimensions } from 'react-native';
 import {
   GestureHandlerRootView,
   PanGestureHandler,
@@ -12,9 +11,6 @@ import { useRecoilState } from 'recoil';
 type Props = {
   children: React.ReactNode;
 };
-
-const { width: screenWidth } = Dimensions.get('window');
-const halfScreenWidth = screenWidth / 4;
 /**
  * 左右のスワイプを検知するコンポーネント。
  */
@@ -35,8 +31,10 @@ export const GestureHandlerView: FC<Props> = ({ children }) => {
     if (nativeEvent.oldState === State.ACTIVE) {
       const { translationX } = nativeEvent;
 
+      console.log('translationX', translationX);
+
       // 右にスワイプされた場合、selectItems から 現在選択されているカテゴリー の前のカテゴリーを取得する
-      if (translationX > halfScreenWidth) {
+      if (translationX > 30) {
         selectItems.find((item, index) => {
           if (item === selectFridgeCategory) {
             setSelectFridgeCategory(
@@ -44,16 +42,14 @@ export const GestureHandlerView: FC<Props> = ({ children }) => {
             );
           }
         });
-        Alert.alert('Swiped Right', 'You swiped right!');
       }
       // 左にスワイプされた場合、selectItems から 現在選択されているカテゴリー の次のカテゴリーを取得する
-      if (translationX < -halfScreenWidth) {
+      if (translationX < -30) {
         selectItems.find((item, index) => {
           if (item === selectFridgeCategory) {
             setSelectFridgeCategory(selectItems[index + 1] ?? selectItems[0]);
           }
         });
-        Alert.alert('Swiped Left', 'You swiped left!');
       }
     }
   };
@@ -62,7 +58,7 @@ export const GestureHandlerView: FC<Props> = ({ children }) => {
     <GestureHandlerRootView style={{ flex: 1 }}>
       <PanGestureHandler
         onHandlerStateChange={onSwipe}
-        activeOffsetX={[-halfScreenWidth, halfScreenWidth]}
+        activeOffsetX={[-30, 30]}
       >
         {children}
       </PanGestureHandler>
