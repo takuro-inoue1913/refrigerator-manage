d0e0189f9cae7620c2f50342f9a27405a42418f1
takuro-inoue <inoue.takuro@moneyforward.co.jp>
2024-01-10 10:28:07 +0900

    Refactor useTypedNavigation hook

--- src/hooks/useTypedNavigation.ts
+++ src/hooks/useTypedNavigation.ts
@@ -1,21 +1,25 @@
 import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { RootStackParamList } from '@src/types';
+import { useCallback } from 'react';
 
 export function useTypedNavigation<T extends keyof RootStackParamList>() {
   const navigation = useNavigation<NavigationProp<RootStackParamList, T>>();
 
-  const navigate = (screen: T, params?: RootStackParamList[T]) => {
-    if (params) {
-      const _navigationNavigate = navigation.navigate as (
-        screen: T,
-        params: RootStackParamList[T],
-      ) => void;
-      _navigationNavigate(screen, params);
-    } else {
-      const _navigationNavigate = navigation.navigate as (screen: T) => void;
-      _navigationNavigate(screen);
-    }
-  };
+  const navigate = useCallback(
+    (screen: T, params?: RootStackParamList[T]) => {
+      if (params) {
+        const _navigationNavigate = navigation.navigate as (
+          screen: T,
+          params: RootStackParamList[T],
+        ) => void;
+        _navigationNavigate(screen, params);
+      } else {
+        const _navigationNavigate = navigation.navigate as (screen: T) => void;
+        _navigationNavigate(screen);
+      }
+    },
+    [navigation],
+  );
 
   return { navigate };
 }
