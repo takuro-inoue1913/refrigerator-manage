4aa7753d222954a8a5c75918c807c2fba21cb98d
takuro-inoue <inoue.takuro@moneyforward.co.jp>
2024-01-17 20:48:27 +0900

    Add deleteUserImage function to delete user images from Firebase Storage

---
+++ src/interface/firebase/deleteUserImage.ts
@@ -0,0 +1,29 @@
+import { ref, deleteObject } from 'firebase/storage';
+import { storage } from '@src/utils/firebaseSettings';
+
+/**
+ * Firebase Storage から指定されたパスの画像ファイルを削除する。
+ * @param {string} storagePath - 削除するファイルがあるStorage内のパス
+ */
+export const deleteUserImage = async (storagePath: string): Promise<void> => {
+  // firebase の Storage URL からパスを抽出
+  const matches = storagePath.match(/o\/(.+?)\?alt=media/);
+  if (!matches || matches.length < 2) {
+    throw new Error('Invalid storage path');
+  }
+
+  // 抽出したパスをデコード
+  const url = decodeURIComponent(matches[1]);
+
+  // Storageの参照を作成
+  const storageRef = ref(storage, url);
+
+  // ファイルを削除
+  try {
+    await deleteObject(storageRef);
+    Promise.resolve();
+  } catch (error) {
+    Promise.reject(error);
+    console.error('There was an error deleting the image:', error);
+  }
+};
