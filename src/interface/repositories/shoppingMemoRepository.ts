547ab419ce31c5f6a4f09238704502ca7d1ca6e0
takuro-inoue <inoue.takuro@moneyforward.co.jp>
2024-01-25 20:54:17 +0900

    Add insert, update, and delete methods to shoppingMemoRepository

--- src/interface/repositories/shoppingMemoRepository.ts
+++ src/interface/repositories/shoppingMemoRepository.ts
@@ -1,4 +1,9 @@
-import { GetShoppingMemosDocument } from '@src/interface/__generated__/graphql';
+import {
+  GetShoppingMemosDocument,
+  InsertShoppingMemoDocument,
+  UpdateShoppingMemoDocument,
+  DeleteShoppingMemoDocument,
+} from '@src/interface/__generated__/graphql';
 import { buildGraphQLUserClient } from '@src/interface/logics/buildGraphQLClient/buildGraphQLUserClient';
 
 export const shoppingMemoRepository = {
@@ -6,6 +11,60 @@ export const shoppingMemoRepository = {
     const client = buildGraphQLUserClient(idToken);
 
     const data = await client.request(GetShoppingMemosDocument);
+    return data;
+  },
+  insert: async ({
+    idToken,
+    masterId,
+    userId,
+    quantity,
+  }: {
+    idToken: string | null;
+    masterId: string;
+    userId: string;
+    quantity: number;
+  }) => {
+    const client = buildGraphQLUserClient(idToken);
+
+    const data = await client.request(InsertShoppingMemoDocument, {
+      masterId,
+      userId,
+      quantity,
+    });
+    return data;
+  },
+  update: async ({
+    idToken,
+    shoppingMemoId,
+    masterId,
+    quantity,
+  }: {
+    idToken: string | null;
+    shoppingMemoId: string;
+    masterId: string;
+    quantity: number;
+  }) => {
+    const client = buildGraphQLUserClient(idToken);
+
+    const data = await client.request(UpdateShoppingMemoDocument, {
+      shoppingMemoId,
+      masterId,
+      quantity,
+    });
+    return data;
+  },
+  delete: async ({
+    idToken,
+    shoppingMemoId,
+  }: {
+    idToken: string | null;
+    shoppingMemoId: string;
+  }) => {
+    const client = buildGraphQLUserClient(idToken);
+
+    const data = await client.request(DeleteShoppingMemoDocument, {
+      shoppingMemoId,
+    });
     return data;
   },
 };
