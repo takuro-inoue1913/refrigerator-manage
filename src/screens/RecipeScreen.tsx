726c91c4a805e4f1cf0fd7263d0a01123035c20f
takuro-inoue <inoue.takuro@moneyforward.co.jp>
2024-02-03 21:44:11 +0900

    Add react-native-calendars library and implement Japanese locale support

--- src/screens/RecipeScreen.tsx
+++ src/screens/RecipeScreen.tsx
@@ -1,6 +1,70 @@
-import React, { FC } from 'react';
-import { Text } from 'react-native';
+import React from 'react';
+import dayjs from 'dayjs';
+import { CalendarList, LocaleConfig } from 'react-native-calendars';
 
-export const RecipeScreen: FC = () => {
-  return <Text>RecipeScreen</Text>;
+LocaleConfig.locales['jp'] = {
+  monthNames: [
+    '1月',
+    '2月',
+    '3月',
+    '4月',
+    '5月',
+    '6月',
+    '7月',
+    '8月',
+    '9月',
+    '10月',
+    '11月',
+    '12月',
+  ],
+  monthNamesShort: [
+    '1月',
+    '2月',
+    '3月',
+    '4月',
+    '5月',
+    '6月',
+    '7月',
+    '8月',
+    '9月',
+    '10月',
+    '11月',
+    '12月',
+  ],
+  dayNames: [
+    '日曜日',
+    '月曜日',
+    '火曜日',
+    '水曜日',
+    '木曜日',
+    '金曜日',
+    '土曜日',
+  ],
+  dayNamesShort: ['日', '月', '火', '水', '木', '金', '土'],
+  today: '今日',
+};
+LocaleConfig.defaultLocale = 'jp';
+
+export const RecipeScreen = () => {
+  return (
+    <CalendarList
+      // 縦スクロールを有効化
+      horizontal={false}
+      // 無限スクロールを有効化
+      pagingEnabled={true}
+      // 現在の月から何ヶ月前まで表示するか
+      pastScrollRange={12}
+      // 現在の月から何ヶ月後まで表示するか
+      futureScrollRange={12}
+      // スクロール時に月が変わったことを検知するコールバック
+      onVisibleMonthsChange={(months) => {
+        console.log('now these months are visible', months);
+      }}
+      // 現在の日付をマーク
+      markedDates={{
+        [dayjs().format('YYYY-MM-DD')]: { selected: true },
+      }}
+      // その他のカレンダーオプション...
+    />
+  );
 };
