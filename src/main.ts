7462729bde15e03f000c2b5bc9cb2af2928ad285
takuro-inoue <inoue.takuro@moneyforward.co.jp>
2024-02-05 22:16:51 +0900

    Add Japanese localization for calendar

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,49 @@
 import registerRootComponent from 'expo/build/launch/registerRootComponent';
+import { LocaleConfig } from 'react-native-calendars';
 import { AppWrapper } from '@src/index';
 
 registerRootComponent(AppWrapper);
+
+// カレンダーの日本語化設定
+LocaleConfig.locales['jp'] = {
+  monthNames: [
+    '1月',
+    '2月',
+    '3月',
+    '4月',
+    '5月',
+    '6月',
+    '7月',
+    '8月',
+    '9月',
+    '10月',
+    '11月',
+    '12月',
+  ],
+  monthNamesShort: [
+    '1月',
+    '2月',
+    '3月',
+    '4月',
+    '5月',
+    '6月',
+    '7月',
+    '8月',
+    '9月',
+    '10月',
+    '11月',
+    '12月',
+  ],
+  dayNames: [
+    '日曜日',
+    '月曜日',
+    '火曜日',
+    '水曜日',
+    '木曜日',
+    '金曜日',
+    '土曜日',
+  ],
+  dayNamesShort: ['日', '月', '火', '水', '木', '金', '土'],
+  today: '今日',
+};
+LocaleConfig.defaultLocale = 'jp';
--- src/screens/RecipeScreen.tsx
+++ src/screens/RecipeScreen.tsx
@@ -1,70 +1,24 @@
 import React from 'react';
 import dayjs from 'dayjs';
-import { CalendarList, LocaleConfig } from 'react-native-calendars';
-
-LocaleConfig.locales['jp'] = {
-  monthNames: [
-    '1月',
-    '2月',
-    '3月',
-    '4月',
-    '5月',
-    '6月',
-    '7月',
-    '8月',
-    '9月',
-    '10月',
-    '11月',
-    '12月',
-  ],
-  monthNamesShort: [
-    '1月',
-    '2月',
-    '3月',
-    '4月',
-    '5月',
-    '6月',
-    '7月',
-    '8月',
-    '9月',
-    '10月',
-    '11月',
-    '12月',
-  ],
-  dayNames: [
-    '日曜日',
-    '月曜日',
-    '火曜日',
-    '水曜日',
-    '木曜日',
-    '金曜日',
-    '土曜日',
-  ],
-  dayNamesShort: ['日', '月', '火', '水', '木', '金', '土'],
-  today: '今日',
-};
-LocaleConfig.defaultLocale = 'jp';
+import { Calendar } from 'react-native-calendars';
+import { COMMON_COLOR_GREEN } from '@src/utils/consts';
 
 export const RecipeScreen = () => {
   return (
-    <CalendarList
-      // 縦スクロールを有効化
+    <Calendar
       horizontal={false}
-      // 無限スクロールを有効化
       pagingEnabled={true}
-      // 現在の月から何ヶ月前まで表示するか
       pastScrollRange={12}
-      // 現在の月から何ヶ月後まで表示するか
       futureScrollRange={12}
-      // スクロール時に月が変わったことを検知するコールバック
       onVisibleMonthsChange={(months) => {
         console.log('now these months are visible', months);
       }}
-      // 現在の日付をマーク
+      enableSwipeMonths={true}
       markedDates={{
-        [dayjs().format('YYYY-MM-DD')]: { selected: true },
+        [dayjs().format('YYYY-MM-DD')]: {
+          textColor: COMMON_COLOR_GREEN,
+        },
       }}
-      // その他のカレンダーオプション...
     />
   );
 };
