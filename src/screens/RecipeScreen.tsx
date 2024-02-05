6976e5c2cf332fd4303c407bd0c6777ec2b56045
takuro-inoue <inoue.takuro@moneyforward.co.jp>
2024-02-05 22:20:08 +0900

    Add selected date functionality to RecipeScreen

--- src/screens/RecipeScreen.tsx
+++ src/screens/RecipeScreen.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import dayjs from 'dayjs';
 import { Calendar } from 'react-native-calendars';
 import { COMMON_COLOR_GREEN } from '@src/utils/consts';
+import { Text, View } from 'react-native';
 
 export const RecipeScreen = () => {
+  const [selectedDate, setSelectedDate] = useState(
+    dayjs().format('YYYY-MM-DD'),
+  );
   return (
-    <Calendar
-      horizontal={false}
-      pagingEnabled={true}
-      pastScrollRange={12}
-      futureScrollRange={12}
-      onVisibleMonthsChange={(months) => {
-        console.log('now these months are visible', months);
-      }}
-      enableSwipeMonths={true}
-      markedDates={{
-        [dayjs().format('YYYY-MM-DD')]: {
-          textColor: COMMON_COLOR_GREEN,
-        },
-      }}
-    />
+    <View>
+      <Calendar
+        horizontal={false}
+        pagingEnabled={true}
+        pastScrollRange={12}
+        futureScrollRange={12}
+        onVisibleMonthsChange={(months) => {
+          console.log('now these months are visible', months);
+        }}
+        enableSwipeMonths={true}
+        markedDates={{
+          [dayjs().format('YYYY-MM-DD')]: {
+            textColor: COMMON_COLOR_GREEN,
+          },
+          [selectedDate]: {
+            selected: true,
+            selectedColor: COMMON_COLOR_GREEN,
+          },
+        }}
+        onDayPress={(day) => {
+          setSelectedDate(day.dateString);
+        }}
+      />
+      <View>
+        <Text>RecipeScreen</Text>
+      </View>
+    </View>
   );
 };
