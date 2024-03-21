b041bbe8c841671afce3a946564fe5f091d35ceb
takuro-inoue1913 <takuro.inoue1988@gmail.com>
2024-03-21 21:25:40 +0900

    Update Apollo config with new service name and URL

--- apollo.config.js
+++ apollo.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   client: {
     service: {
-      name: 'takuro-inoues-Team@current',
-      url: 'https://current--takuro-inoues-team.apollographos.net/graphql',
+      name: 'Frimane-GraphQL-Schema@current',
+      url: 'https://current--frimane-graphql-schema.apollographos.net/graphql',
       headers: {
         'x-hasura-admin-secret': `${process.env.HASURA_ADMIN_SECRET}`,
       },
