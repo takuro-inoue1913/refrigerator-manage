46b1c8328dd3d4407c64ccc727101d0f5dc78bb4
takuro-inoue1913 <takuro.inoue1988@gmail.com>
2024-05-04 17:07:48 +0900

    Refactor Apollo config and update import statements

--- apollo.config.js
+++ apollo.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   client: {
     service: {
-      name: 'frimane',
-      url: 'https://frimane.hasura.app/v1/graphql',
+      name: 'Frimane-GraphQL-Schem-s0ry8@current',
+      url: 'https://current--frimane-graphql-schem-s0ry8.apollographos.net/graphql',
       headers: {
         'x-hasura-admin-secret': `${process.env.HASURA_ADMIN_SECRET}`,
       },
