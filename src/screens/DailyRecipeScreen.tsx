a82aef420dae11876e0c7d08103af018224beea7
takuro-inoue1913 <takuro.inoue1988@gmail.com>
2024-05-07 21:57:33 +0900

    refactor: Remove unused code and update styling in DailyRecipeScreen

--- src/screens/DailyRecipeScreen.tsx
+++ src/screens/DailyRecipeScreen.tsx
@@ -359,9 +359,6 @@ export const DailyRecipeScreen = () => {
         pagingEnabled={true}
         pastScrollRange={12}
         futureScrollRange={12}
-        onVisibleMonthsChange={(months) => {
-          console.log('now these months are visible', months);
-        }}
         enableSwipeMonths={true}
         markedDates={{
           [dayjs().format('YYYY-MM-DD')]: {
