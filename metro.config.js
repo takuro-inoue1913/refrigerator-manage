cd4e720938846aef134802322812d24f33afea73
takuro-inoue1913 <takuro.inoue1988@gmail.com>
2024-06-02 15:18:01 +0900

    refactor: Update metro.config.js to use expo/metro-config for default configuration

--- metro.config.js
+++ metro.config.js
@@ -1,33 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require('path');
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
 
-const defaultConfig = getDefaultConfig(__dirname);
-const {
-  resolver: { sourceExts, assetExts },
-} = defaultConfig;
+const config = getDefaultConfig(__dirname);
 
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {
-  transformer: {
-    babelTransformerPath: require.resolve('react-native-svg-transformer'),
-    inlineRequires: true,
-  },
-  resolver: {
-    extraNodeModules: {
-      '@src': path.resolve(__dirname, 'src'),
-    },
-    assetExts: assetExts.filter((ext) => ext !== 'svg'),
-    sourceExts: [...sourceExts, 'svg'],
-    resolverMainFields: ['sbmodern', 'react-native', 'browser', 'main'],
-  },
-  watchFolders: [path.resolve(__dirname, 'src')],
-};
-
-module.exports = mergeConfig(defaultConfig, config);
+module.exports = config;
